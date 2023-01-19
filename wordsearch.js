4d88d42023089a99c91158a61c51cff145f0bcce
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-19 01:12:08 +0000

    linted

--- wordsearch.js
+++ wordsearch.js
@@ -1,24 +1,24 @@
 const transpose = require('./transpose.js');
 
-const wordSearch = (letters, word) => { 
-    if (word === undefined) {
-        return false;
-    }
-    const upperCase = word.toUpperCase();
+const wordSearch = (letters, word) => {
+  if (word === undefined) {
+    return false;
+  }
+  const upperCase = word.toUpperCase();
     
-    if (upperCase === "") {
-        return false;
-    }
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(upperCase)) return true
-    }
-    const verticalLetters = transpose(letters);
-    const verticalJoin = verticalLetters.map(ls => ls.join(''));
-    for (l of verticalJoin) {
-        if (l.includes(upperCase)) return true
-    }
-return false;
-}
+  if (upperCase === "") {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(upperCase)) return true;
+  }
+  const verticalLetters = transpose(letters);
+  const verticalJoin = verticalLetters.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(upperCase)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
